1443870a5aba4588a3fbe0113828f519b2c63d99
harish <harish@harry-pc.(none)>
2013-05-27 18:48:15 +0530

    Changed the disconnect message for the user.

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ io.sockets.on('connection', function(socket) {
 	socket.on('disconnect', function() {
 		var i;
 		for(i in clients) {
-			clients[i].disconnect();
+			clients[i].disconnect("Scrollback says bye..........");
 			delete clients[i]; // Speed up GC?
 		}
 	});
